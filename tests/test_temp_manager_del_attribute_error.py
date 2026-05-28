2239d06adc7d0cedd6dded4543ef7d4bafc25fba
tedaks <entomard@gmail.com>
2026-05-28 18:41:42 +0800

    fix: move TempDirManager.__del__ attribute check inside try block to prevent UnraisableException

---
+++ tests/test_temp_manager_del_attribute_error.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2026 Bortre Tenamo <tedaks@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Regression test: TempDirManager.__del__ must not raise on uninitialized instance."""
+
+
+def test_temp_manager_del_without_init():
+    """__del__ must not raise when __init__ was never called (AttributeError on self._dirs)."""
+    from NoWires.temp_manager import TempDirManager
+
+    obj = TempDirManager.__new__(TempDirManager)
+    try:
+        obj.__del__()
+    except AttributeError:
+        assert False, "TempDirManager.__del__ must not raise on uninitialized instance"
+
+
+def test_temp_manager_del_after_partial_init():
+    """__del__ must not raise when only some attributes are set."""
+    from NoWires.temp_manager import TempDirManager
+
+    obj = TempDirManager.__new__(TempDirManager)
+    obj._dirs = []
+    try:
+        obj.__del__()
+    except AttributeError:
+        assert False, "TempDirManager.__del__ must not raise with partial init"
